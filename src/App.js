23a044ba4f084f074f051f99821f27c0a46a342b
Yudi Runat Masneno <yudi.berland@gmail.com>
2023-08-11 10:36:10 +0700

    map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
+import "@reach/combobox/styles.css";
+import PlacesAutocomplete from "./PlacesAutocomplete";
 function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyB37WjSx3gW4BjHGkGgbgpAgMhipXWr8oY",
+    libraries: ["places"],
+  });
+  if (loadError) return <div>Error loading maps</div>;
+
+  let datasMap = !isLoaded ? <div>Loading...</div> : <Map />;
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return datasMap;
+}
+
+function Map() {
+  const center = { lat: -6.2087634, lng: 106.845599 };
+
+  const [selected, setSelected] = useState(null);
+  const [markerPosition, setMarkerPosition] = useState(null);
+
+  const onMarkerDragEnd = (event) => {
+    setMarkerPosition({
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng(),
+    });
+  };
+  console.log(markerPosition);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="places-container">
+        <PlacesAutocomplete
+          setMarkerPosition={setMarkerPosition}
+          setSelected={setSelected}
+        />
+      </div>
+      <GoogleMap
+        zoom={!selected ? 10 : 18}
+        center={!selected ? center : selected}
+        mapContainerClassName="map-container"
+      >
+        {selected && (
+          <Marker onDragEnd={onMarkerDragEnd} position={selected} draggable />
+        )}
+      </GoogleMap>
+    </>
   );
 }
 
